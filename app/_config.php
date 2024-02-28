0149cb68962ddce7a4a52b6c2c1c7bb790047f73
cdogcatch <cameron@catchdesign.co.nz>
2024-02-28 16:17:31 +1300

    maybe this for ssl?

--- app/_config.php
+++ app/_config.php
@@ -4,4 +4,5 @@ use SilverStripe\Control\Director;
 
 if (!Director::isDev()) {
     // Director::forceSSL();
+    Director::config()->set('alternate_base_url', 'https://elp-bookings-c35eo.ondigitalocean.app');
 }
